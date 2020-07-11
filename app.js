c3554b2f5c75c1fe1409c65e0157f49e79f39a45
Bisma <Bisma.Naz99@gmail.com>
2020-07-11 21:04:38 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+var hour=0;
+var min=0;
+var sec=0;
+var msec=0;
+
+var hourHeading=document.getElementById("hour");
+var minHeading=document.getElementById("min");
+var secHeading=document.getElementById("sec");
+var msecHeading=document.getElementById("msec");
+
+var interval;
+
+function timer(){
+    msec++
+    msecHeading.innerHTML=msec;
+    if(msec >= 100){
+        sec++;
+        secHeading.innerHTML=sec;
+        msec = 0;
+        if(sec >=60){
+            min++;
+            minHeading.innerHTML=min;
+            sec = 0;
+        }
+        if(min >=59){
+            hour++;
+            hourHeading.innerHTML=hour;
+            min = 0;
+        }
+    }
+}
+
+function start(){
+
+interval = setInterval(timer,10);
+
+}
+
+function stop(){
+    clearInterval(interval); 
+}
+
+function reset(){
+    min=0;
+    sec=0;
+    msec=0;
+    minHeading.innerHTML=min;
+    secHeading.innerHTML=sec;
+    msecHeading.innerHTML=msec;
+    stop();
+}
+
+function btnDisable(){
+    var btn = document.getElementById('start').disabled = true;
+    
+}
+
+function btnVisible(){
+    var btn =document.getElementById('start').disabled = false;
+    
+}
